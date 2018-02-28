81ee84c8ff7a1b96d17bc90c9022b8ebdd0eee27
Michal Nowakowski <michal@nowakowski.me.uk>
2018-02-28 11:43:05 +0100

    Cwiczenie 3

--- src/main/java/com/infoshareacademy/model/Student.java
+++ src/main/java/com/infoshareacademy/model/Student.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.model;
 
+import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -21,12 +22,22 @@ public class Student {
     @NotNull
     private String name;
 
+    @Column(name = "surname")
+    @NotNull
+    private String surname;
+
+    @Column(name = "date_of_birth")
+    @NotNull
+    private LocalDate dateOfBirth;
+
     public Student() {
 
     }
 
-    public Student(String name) {
+    public Student(String name, String surname, LocalDate dateOfBirth) {
         this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
     }
 
     public Long getId() {
@@ -45,11 +56,29 @@ public class Student {
         this.name = name;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("Student{");
         sb.append("id=").append(id);
         sb.append(", name='").append(name).append('\'');
+        sb.append(", surname='").append(surname).append('\'');
+        sb.append(", dateOfBirth=").append(dateOfBirth);
         sb.append('}');
         return sb.toString();
     }
--- src/main/java/com/infoshareacademy/web/StudentServlet.java
+++ src/main/java/com/infoshareacademy/web/StudentServlet.java
@@ -3,6 +3,7 @@ package com.infoshareacademy.web;
 import com.infoshareacademy.dao.StudentDao;
 import com.infoshareacademy.model.Student;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.List;
 import javax.inject.Inject;
@@ -29,8 +30,10 @@ public class StudentServlet extends HttpServlet {
 
         // Test data
         // Students
-        studentDao.save(new Student("Michal"));
-        studentDao.save(new Student("Marek"));
+        studentDao.save(
+            new Student("Michal", "Nowak", LocalDate.of(2000, 2, 14)));
+        studentDao.save(
+            new Student("Marek", "Kowalski", LocalDate.of(1989, 12, 24)));
 
         LOG.info("System time zone is: {}", ZoneId.systemDefault());
     }
@@ -69,6 +72,8 @@ public class StudentServlet extends HttpServlet {
             LOG.info("No Student found for id = {}, nothing to be updated", id);
         } else {
             existingStudent.setName(req.getParameter("name"));
+            existingStudent.setSurname(req.getParameter("surname"));
+            existingStudent.setDateOfBirth(LocalDate.parse(req.getParameter("dateOfBirth")));
 
             studentDao.update(existingStudent);
             LOG.info("Student object updated: {}", existingStudent);
@@ -83,6 +88,8 @@ public class StudentServlet extends HttpServlet {
 
         final Student p = new Student();
         p.setName(req.getParameter("name"));
+        p.setSurname(req.getParameter("surname"));
+        p.setDateOfBirth(LocalDate.parse(req.getParameter("dateOfBirth")));
 
         studentDao.save(p);
         LOG.info("Saved a new Student object: {}", p);
